eb083d5ee38b47823a1dc42fad96830947f60744
Joshua Baskar <jbmsacps@gmail.com>
2026-03-15 12:01:38 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const notesRouter = require('./routes/notes');
+app.use('/api/notes', notesRouter);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.error('MongoDB connection error:', err));
